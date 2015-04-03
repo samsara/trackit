4aa2526ef7032dee9fadb38bb80cbfad19a1c01b
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-04-03 18:47:54 +0100

    release v0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject samsara/trackit "0.1.0-SNAPSHOT"
+(defproject samsara/trackit "0.1.0"
   :description "TRACKit! A Clojure wrapper for Yammer Metric library"
   :url "https://github.com/samsara/trackit"
   :license {:name "Apache License 2.0"
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [metrics-clojure "2.5.0"]
                  [metrics-clojure-graphite "2.5.0"]
-                 #_[metrics-clojure-ganglia  "2.5.0"]  ;; bug #71
+                 #_[metrics-clojure-ganglia  "2.5.0"] ;; bug #71
                  [io.dropwizard.metrics/metrics-ganglia "3.1.1"]
                  [info.ganglia.gmetric4j/gmetric4j "1.0.10"]
                  [riemann-clojure-client "0.3.2"]
@@ -15,4 +15,6 @@
                  ;;[com.bealetech/metrics-statsd "3.0.0a"] ;; version not yet released
                  ]
 
-  :plugins [[lein-marginalia "0.8.0"]])
+  :plugins [[lein-marginalia "0.8.0"]]
+
+  :deploy-repositories[["clojars" {:url "https://clojars.org/repo/"                                                       :sign-releases false}]])
