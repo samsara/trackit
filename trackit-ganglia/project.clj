500e019b31d4449e28a92358eb570c47a8e501c8
Bruno Bonacci <bruno.bonacci@gmail.com>
2016-09-11 00:19:06 +0100

    adding reporter as module

---
+++ trackit-ganglia/project.clj
@@ -0,0 +1,13 @@
+(defproject samsara/trackit-ganglia (-> "../trackit.version" slurp .trim)
+
+  :description
+  "TRACKit! A Clojure developer friendly wrapper for Yammer's Metrics library."
+
+  :url "https://github.com/samsara/trackit"
+
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+
+  :dependencies [#_[metrics-clojure-ganglia  "2.5.0"] ;; bug #71
+                 [io.dropwizard.metrics/metrics-ganglia "3.1.1"]
+                 [info.ganglia.gmetric4j/gmetric4j "1.0.10"]])
