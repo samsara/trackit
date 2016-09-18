34249e94b822dee5388e965217aaa33bf135622f
Bruno Bonacci <bruno.bonacci@gmail.com>
2016-09-18 23:32:00 +0100

    statsd reporter fix

--- trackit-statsd/src/samsara/trackit/reporter_statsd.clj
+++ trackit-statsd/src/samsara/trackit/reporter_statsd.clj
@@ -1,98 +1,20 @@
-(ns samsara.trackit.reporter
+(ns samsara.trackit.reporter-statsd
   (:import  [java.util.concurrent TimeUnit]
             [com.codahale.metrics MetricFilter])
-  (:require [metrics.reporters.console :as console]
-            #_[metrics.reporters.ganglia :as ganglia])
-  #_(:require [metrics.reporters.graphite :as graphite]
-            #_[metrics.reporters.ganglia :as ganglia])
-  #_(:import  [com.codahale.metrics.ganglia GangliaReporter]
-            [info.ganglia.gmetric4j.gmetric GMetric GMetric$UDPAddressingMode])
-  #_(:import  [com.codahale.metrics.riemann RiemannReporter Riemann]
-            [com.aphyr.riemann.client RiemannClient TcpTransport])
-
-  #_(:import  [com.bealetech.metrics.reporting StatsdReporter Statsd]))
-
-(defmulti start-reporting (fn [registry cfg] (:type cfg)))
+  (:require [samsara.trackit.reporter])
+  (:import  [com.bealetech.metrics.reporting StatsdReporter Statsd]))
 
 
-
-(defmethod start-reporting :default [registry cfg]
-  (println "TRACKit!: no reporting method selected."))
-
-
-
-(defmethod start-reporting :console
+(defmethod samsara.trackit.reporter/start-reporting :statsd
   [registry
-   {:keys [reporting-frequency-seconds stream rate-unit duration-unit]
-    :or  {reporting-frequency-seconds 300, stream (System/err)
+   {:keys [reporting-frequency-seconds host port prefix rate-unit duration-unit]
+    :or  {reporting-frequency-seconds 10, host "localhost", port 8125, prefix "trackit"
           rate-unit TimeUnit/SECONDS, duration-unit TimeUnit/MILLISECONDS} :as cfg}]
-  (console/start
-   (console/reporter registry cfg)
-   reporting-frequency-seconds))
 
-
-(comment
-  (defmethod start-reporting :graphite
-    [registry
-     {:keys [reporting-frequency-seconds host port prefix rate-unit duration-unit]
-      :or  {reporting-frequency-seconds 10, host "localhost", port 2003, prefix "trackit"
-            rate-unit TimeUnit/SECONDS, duration-unit TimeUnit/MILLISECONDS} :as cfg}]
-    (require 'metrics.reporters.graphite)
-    (graphite/start
-     (graphite/reporter registry cfg)
-     reporting-frequency-seconds)))
-
-(comment
-  (defmethod start-reporting :statsd
-    [registry
-     {:keys [reporting-frequency-seconds host port prefix rate-unit duration-unit]
-      :or  {reporting-frequency-seconds 10, host "localhost", port 8125, prefix "trackit"
-            rate-unit TimeUnit/SECONDS, duration-unit TimeUnit/MILLISECONDS} :as cfg}]
-
-    (-> (StatsdReporter/forRegistry registry)
-        (.prefixedWith prefix)
-        (.convertDurationsTo duration-unit)
-        (.convertRatesTo rate-unit)
-        (.filter MetricFilter/ALL)
-        (.build (Statsd. host port))
-        (.start reporting-frequency-seconds TimeUnit/SECONDS))))
-
-
-(comment
-  (defmethod start-reporting :riemann
-    [registry
-     {:keys [reporting-frequency-seconds host port prefix rate-unit duration-unit]
-      :or  {reporting-frequency-seconds 10, host "localhost", port 5555, prefix "trackit"
-            rate-unit TimeUnit/SECONDS, duration-unit TimeUnit/MILLISECONDS} :as cfg}]
-
-    (let [;; tell Riemann client to do not cache the dns name
-          rc (Riemann.
-              (RiemannClient.
-               (doto
-                   (TcpTransport. host (int port))
-                 (-> .-cacheDns (.set false)))))]
-      (-> (RiemannReporter/forRegistry registry)
-          (.prefixedWith prefix)
-          (.convertDurationsTo duration-unit)
-          (.convertRatesTo rate-unit)
-          (.useSeparator ".")
-          (.filter MetricFilter/ALL)
-          (.build rc)
-          (.start reporting-frequency-seconds TimeUnit/SECONDS))))
-
-
-
-  (defmethod start-reporting :ganglia
-    [registry
-     {:keys [reporting-frequency-seconds host port prefix rate-unit duration-unit]
-      :or  {reporting-frequency-seconds 60, host "localhost", port 8649, prefix "trackit"
-            rate-unit TimeUnit/SECONDS, duration-unit TimeUnit/MILLISECONDS} :as cfg}]
-
-    (-> (GangliaReporter/forRegistry registry)
-        (.prefixedWith prefix)
-        (.convertDurationsTo duration-unit)
-        (.convertRatesTo rate-unit)
-        (.filter MetricFilter/ALL)
-        (.build (GMetric. host (int port) GMetric$UDPAddressingMode/MULTICAST 1))
-        (.start reporting-frequency-seconds TimeUnit/SECONDS)))
-  )
+  (-> (StatsdReporter/forRegistry registry)
+      (.prefixedWith prefix)
+      (.convertDurationsTo duration-unit)
+      (.convertRatesTo rate-unit)
+      (.filter MetricFilter/ALL)
+      (.build (Statsd. host port))
+      (.start reporting-frequency-seconds TimeUnit/SECONDS)))
