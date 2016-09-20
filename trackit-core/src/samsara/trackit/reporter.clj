f6447bf551dfce7001e236a98d13d0c9ab9a8d15
Bruno Bonacci <bruno.bonacci@gmail.com>
2016-09-20 23:16:00 +0100

    dynamic load of reporters

--- trackit-core/src/samsara/trackit/reporter.clj
+++ trackit-core/src/samsara/trackit/reporter.clj
@@ -7,9 +7,36 @@
 (defmulti start-reporting (fn [registry cfg] (:type cfg)))
 
 
+(defn- load-function-from-name
+  ([fqn-fname]
+   (if (string? fqn-fname)
+     (let [[_ fns ff] (re-find #"([^/]+)/([^/]+)" fqn-fname)]
+       (when (not (and fns ff))
+         (throw (ex-info (str "function '" fqn-fname "' is invalid format. must be \"namespace/fun-name\".") {})))
+       (load-function-from-name fns ff))
+     fqn-fname))
+  ([fn-ns fn-name]
+   (when (not (and fn-ns fn-name))
+     (throw (ex-info (str "function '" fn-ns "/" fn-name "' not found.") {})))
+   ;; requiring the namespace
+   (require (symbol fn-ns))
+   (let [fn-symbol (resolve (symbol fn-ns fn-name))]
+     (when-not fn-symbol
+       (throw (ex-info (str "function '" fn-ns "/" fn-name "' not found.") {})))
+     fn-symbol)))
+
+
 
 (defmethod start-reporting :default [registry cfg]
   (println "TRACKit!: no reporting method selected."))
+
+
+(defmethod start-reporting :custom
+  [registry
+   {:keys [fqn-start-function] :as cfg}]
+
+  (let [reporter (load-function-from-name fqn-start-function)]
+    (reporter registry cfg)))
 
 
 
@@ -30,16 +57,20 @@
     :or  {reporting-frequency-seconds 10, host "localhost", port 2003, prefix "trackit"
           rate-unit TimeUnit/SECONDS, duration-unit TimeUnit/MILLISECONDS} :as cfg}]
 
-)
+  (let [reporter (load-function-from-name "samsara.trackit.reporter-graphite/start-reporting")]
+    (reporter registry cfg)))
 
-(comment
-  (defmethod start-reporting :statsd
-    [registry
-     {:keys [reporting-frequency-seconds host port prefix rate-unit duration-unit]
-      :or  {reporting-frequency-seconds 10, host "localhost", port 8125, prefix "trackit"
-            rate-unit TimeUnit/SECONDS, duration-unit TimeUnit/MILLISECONDS} :as cfg}]
 
-))
+
+(defmethod start-reporting :statsd
+  [registry
+   {:keys [reporting-frequency-seconds host port prefix rate-unit duration-unit]
+    :or  {reporting-frequency-seconds 10, host "localhost", port 8125, prefix "trackit"
+          rate-unit TimeUnit/SECONDS, duration-unit TimeUnit/MILLISECONDS} :as cfg}]
+
+  (let [reporter (load-function-from-name "samsara.trackit.reporter-statsd/start-reporting")]
+    (reporter registry cfg)))
+
 
 
 (defmethod start-reporting :riemann
@@ -48,7 +79,8 @@
     :or  {reporting-frequency-seconds 10, host "localhost", port 5555, prefix "trackit"
           rate-unit TimeUnit/SECONDS, duration-unit TimeUnit/MILLISECONDS} :as cfg}]
 
-)
+  (let [reporter (load-function-from-name "samsara.trackit.reporter-riemann/start-reporting")]
+    (reporter registry cfg)))
 
 
 
@@ -58,4 +90,5 @@
     :or  {reporting-frequency-seconds 60, host "localhost", port 8649, prefix "trackit"
           rate-unit TimeUnit/SECONDS, duration-unit TimeUnit/MILLISECONDS} :as cfg}]
 
-)
+  (let [reporter (load-function-from-name "samsara.trackit.reporter-ganglia/start-reporting")]
+    (reporter registry cfg)))
