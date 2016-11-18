a8046a7a75f8887b7515a991e871ecc554226080
Dayo Oliyide <dayo.oliyide@gmail.com>
2016-11-18 16:47:30 +0000

    Fix: Riemann host not to be used as the source host

--- trackit-riemann/src/samsara/trackit/reporter_riemann.clj
+++ trackit-riemann/src/samsara/trackit/reporter_riemann.clj
@@ -1,15 +1,21 @@
 (ns samsara.trackit.reporter-riemann
   (:import  [java.util.concurrent TimeUnit]
-            [com.codahale.metrics MetricFilter])
+            [com.codahale.metrics MetricFilter]
+            [java.net InetAddress UnknownHostException])
   (:require [metrics.reporters.riemann :as riemann]))
 
+(defn- local-host-name []
+  (try
+    (.getCanonicalHostName (InetAddress/getLocalHost))
+    (catch UnknownHostException uhe
+      "Unknown Hostname")))
 
 (defn start-reporting
   [registry
-   {:keys [reporting-frequency-seconds host port prefix rate-unit duration-unit separator tags]
-    :or  {reporting-frequency-seconds 10, host "localhost", port 5555, prefix "trackit"
-          rate-unit TimeUnit/SECONDS, duration-unit TimeUnit/MILLISECONDS
+   {:keys [local-host reporting-frequency-seconds host port prefix rate-unit duration-unit separator tags]
+    :or  {local-host (local-host-name), reporting-frequency-seconds 10, host "localhost", port 5555
+          prefix "trackit", rate-unit TimeUnit/SECONDS, duration-unit TimeUnit/MILLISECONDS
           separator "." tags {}} :as cfg}]
-  (let [reporter (riemann/reporter (riemann/make-riemann host port) registry (assoc cfg :host-name host))]
+  (let [reporter (riemann/reporter (riemann/make-riemann host port) registry (assoc cfg :host-name local-host))]
     (riemann/start reporter reporting-frequency-seconds)
     (fn [] (riemann/stop reporter))))
