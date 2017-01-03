3e979b3c955a9ad7b4855c7c540b12d67c291336
Dayo Oliyide <dayo.oliyide@gmail.com>
2017-01-03 18:16:58 +0000

    Keeping the name consistent with other metric helper fns
    The user isn't aware of how the name passed to helper fns
    (e.g count-tracker, rate-tracker) end up being manipulated by the
    namer and metrics.core/metric-name functions.
    I think this consistency should apply to this function also.

--- trackit-core/src/samsara/trackit.clj
+++ trackit-core/src/samsara/trackit.clj
@@ -370,12 +370,11 @@
         (map (fn [[k v]] (as-metric k v)))
         (sort-by :name))))
 
-
-
 (defn get-metric
   ([name] (get-metric *registry* name))
   ([^MetricRegistry registry name]
-   (metric-value name (get (.getMetrics registry) name))))
+   (let [m-name (-> name namer m/metric-name)]
+     (metric-value m-name (get (.getMetrics registry) m-name)))))
 
 (defn remove-metric
   ([name] (remove-metric *registry* name))
